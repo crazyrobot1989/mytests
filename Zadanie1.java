5caa2b65a5dde0774631f272eea1c60fb73f71b1
crazyrobot1989 <44597099+crazyrobot1989@users.noreply.github.com>
2018-11-06 06:48:04 +0100

    Add files via upload

---
+++ Zadanie1.java
@@ -0,0 +1,21 @@
+public class Zadanie1 {
+
+    public static void main (String args[])
+
+    {
+        int z1 = 5;
+        int z2 = 15;
+        int wynik = z1+z2;
+        int wynik1 = z1-z2;
+        int wynik2 = z1*z2;
+        float wynik3 = (float) z1/z2;
+        int wynik4 = z1%z2;
+
+        System.out.println("15+5 = " + wynik);
+        System.out.println("15-5 = " + wynik1);
+        System.out.println("15*5 = " + wynik2);
+        System.out.println("15/5 = " + wynik3);
+        System.out.println("15%5 = " + wynik4);
+
+    }
+}
